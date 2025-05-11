fa964a49a5ac802e868b7f264e8163599418d206
Admin <admin@Admins-Air-2.lan>
2025-05-10 21:59:02 -0500

    fix variable

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return(
         <div className='flex justify-center items-center flex-col w-full h-full m-0 p-0'>
           <title>Angie's Cookbook</title>
-          <meta property="og:title" content={recipe.recipeTitle} />
+          <meta property="og:title" content="Angie\'s Cookbook" />
             <meta property="og:description" content="Welcome to Angie\'s Cookbook, a one stop place for all of the best and my favorite recipes" />
             <meta property="og:image" content={logo} />
             <meta property="og:url" content={`https://accookbook.com/`} />
