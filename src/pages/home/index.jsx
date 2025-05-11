b5cb0858ffa8447553c8e7041db54f5905422e20
Admin <admin@Admins-Air-2.lan>
2025-05-10 21:39:10 -0500

    remove and add meta tags

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,15 @@
 import '../../App.css'
 import RecipeList from '../../components/recipes'
+import logo from '../../assets/leaf.png'
 
 export default function Home() {
     return(
         <div className='flex justify-center items-center flex-col w-full h-full m-0 p-0'>
           <title>Angie's Cookbook</title>
-          <description>Welcome to Angie's Cookbook! A culmination of the my favorite recipes.</description>
+          <meta property="og:title" content={recipe.recipeTitle} />
+            <meta property="og:description" content="Welcome to Angie\'s Cookbook, a one stop place for all of the best and my favorite recipes" />
+            <meta property="og:image" content={logo} />
+            <meta property="og:url" content={`https://accookbook.com/`} />
           <RecipeList />
         </div>
     )
